6c672821b21588d1ea9a6f577c8afc7931cce114
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2019-05-23 18:27:36 +0200

    sergio: bugfix.

--- gc3apps/pipelines/imcpipeline.py
+++ gc3apps/pipelines/imcpipeline.py
@@ -67,6 +67,7 @@ class IMCPipeline(StagedTaskCollection):
 
         self.raw_data_destination = self.cfg['experiment']['subfolders']['raw']
         self.metadata = self.cfg['experiment']['subfolders']['scripts']
+        self.subfolders = self.cfg['experiment']['subfolders'].values()        
         if self.cfg['IMC'].has_key('derived_subfolders'):
             self.subfolders += self.cfg['IMC']['derived_subfolders'].values()
 
